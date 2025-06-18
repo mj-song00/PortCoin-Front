0ad2ce6bc057592a61d534c89cedfa44566089f7
mj-song00 <niveus128@gmail.com>
2025-06-18 17:59:34 +0900

    Chore: 주석제거

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Portfolio from "./pages/Portfolio";
+import PortfolioDetail from "./components/PortfolioDetail";
 import Main from "./pages/Main";
-// import PrivateRoute from "./PrivateRoute";
-// import { useAuth } from "./hooks/useAuth";
+import PrivateRoute from "./PrivateRoute"
+import { useAuth } from "./hooks/useAuth";
 
 const App: React.FC = () => {
-  // const { isLoggedIn } = useAuth();
+   const { isLoggedIn, isLoading } = useAuth();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="App">
@@ -16,11 +21,19 @@ const App: React.FC = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route
+          path="/portfolio/:portfolioId"
+          element={
+              <PrivateRoute isLoggedIn={isLoggedIn}>
+            <PortfolioDetail />
+              </PrivateRoute>
+          }
+        />
+        <Route
           path="/portfolio"
           element={
-            //   <PrivateRoute isLoggedIn={isLoggedIn}>
+               <PrivateRoute isLoggedIn={isLoggedIn}>
             <Portfolio />
-            //   </PrivateRoute>
+              </PrivateRoute>
           }
         />
         <Route path="/" element={<Main />} />
